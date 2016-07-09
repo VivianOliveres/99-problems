001db67e4c41ef4167e241af9fa1b9304147d0e2
VivianOliveres <v.oliveres@gmail.com>
2016-07-09 17:46:44 +0200

    P12

--- java8/src/main/java/com/shekhargulati/ninetynine_problems/_01_lists/P12.java
+++ java8/src/main/java/com/shekhargulati/ninetynine_problems/_01_lists/P12.java
@@ -3,9 +3,8 @@ package com.shekhargulati.ninetynine_problems._01_lists;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * <b>(**) Decode a run-length encoded list.</b>
@@ -16,12 +15,15 @@ import static java.util.stream.Collectors.toList;
 public class P12 {
 
     public static <T> List<T> decode(List<Object> encoded) {
-        return encoded.stream().flatMap(e -> {
-            if (e instanceof SimpleEntry) {
-                SimpleEntry<Integer, T> entry = (SimpleEntry<Integer, T>) e;
-                return Collections.nCopies(entry.getKey(), entry.getValue()).stream();
-            }
-            return Stream.of((T) e);
-        }).collect(toList());
+        return encoded.stream()
+                .flatMap(e -> {
+                    if (e instanceof SimpleEntry) {
+                        SimpleEntry<Integer, T> entry = (SimpleEntry<Integer, T>) e;
+                        return Collections.nCopies(entry.getKey(), entry.getValue()).stream();
+                    }
+
+                    return Stream.of((T) e);
+                })
+                .collect(Collectors.toList());
     }
 }
