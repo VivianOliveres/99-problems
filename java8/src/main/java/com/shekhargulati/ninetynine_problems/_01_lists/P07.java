4ba00685f49c1e30afd3875dff1b15238a895d51
VivianOliveres <v.oliveres@gmail.com>
2016-07-09 17:13:20 +0200

    P07

--- java8/src/main/java/com/shekhargulati/ninetynine_problems/_01_lists/P07.java
+++ java8/src/main/java/com/shekhargulati/ninetynine_problems/_01_lists/P07.java
@@ -2,9 +2,8 @@ package com.shekhargulati.ninetynine_problems._01_lists;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * <b>(**) Flatten a nested list structure</b>
@@ -15,22 +14,40 @@ import static java.util.stream.Collectors.toList;
 public class P07 {
 
     public static <T> List<T> flatten(List<?> list, Class<T> elementType) {
-        List<T> flatten = new ArrayList<>();
-        list.forEach(e -> {
-            if (e instanceof List) {
-                flatten.addAll(flatten((List<?>) e, elementType));
+        if (list == null) {
+            return null;
+        }
+
+        List<T> result = new ArrayList<>();
+        for (Object o : list) {
+            if (o instanceof List) {
+                List o1 = (List) o;
+                List<T> flatten = flatten(o1, elementType);
+                result.addAll(flatten);
+
             } else {
-                flatten.add((T) e);
+                T element = elementType.cast(o);
+                result.add(element);
             }
-        });
-        return flatten;
+        }
+
+        return result;
     }
 
     public static <T> List<T> flatten_stream(List<?> list, Class<T> elementType) {
-        return list
-                .stream()
-                .flatMap(e -> e instanceof List ? flatten_stream(((List<?>) e), elementType).stream() : Stream.of(e))
-                .map(e -> (T) e)
-                .collect(toList());
+        if (list == null) {
+            return null;
+        }
+
+        return list.stream()
+                .flatMap(e -> {
+                    if (e instanceof List) {
+                        List o = (List) e;
+                        return flatten_stream(o, elementType).stream();
+                    } else {
+                        return Stream.of(e);
+                    }
+                }).map(o -> elementType.cast(o))
+                .collect(Collectors.toList());
     }
 }
