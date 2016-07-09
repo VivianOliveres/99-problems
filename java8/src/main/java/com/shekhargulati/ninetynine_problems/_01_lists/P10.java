7b0cf4996bbdc77f981bb4c3caa0169ec4786a6c
VivianOliveres <v.oliveres@gmail.com>
2016-07-09 17:28:55 +0200

    P10

--- java8/src/main/java/com/shekhargulati/ninetynine_problems/_01_lists/P10.java
+++ java8/src/main/java/com/shekhargulati/ninetynine_problems/_01_lists/P10.java
@@ -1,9 +1,8 @@
 package com.shekhargulati.ninetynine_problems._01_lists;
 
 import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * <b> (*) Run-length encoding of a list.</b>
@@ -11,6 +10,13 @@ import static java.util.stream.Collectors.toList;
 public class P10 {
 
     public static <T> List<SimpleEntry<Integer, T>> encode(List<T> list) {
-        return P09.pack(list).stream().map(l -> new SimpleEntry<>(l.size(), l.get(0))).collect(toList());
+        List<SimpleEntry<Integer, T>> result = new ArrayList<>();
+        List<List<T>> pack = P09.pack(list);
+        for (List<T> element : pack) {
+            SimpleEntry<Integer, T> entry = new SimpleEntry<>(element.size(), element.get(0));
+            result.add(entry);
+        }
+
+        return result;
     }
 }
